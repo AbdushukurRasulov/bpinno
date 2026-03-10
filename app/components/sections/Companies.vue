2240bbc7b53b1f365c6be19a43b5068623386000
AbdushukurRasulov <abdushukurrasulov1994@gmail.com>
2026-03-10 20:32:40 +0500

    companies slider

--- app/components/sections/Companies.vue
+++ app/components/sections/Companies.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import 'swiper/css';
+
 const companies = [
   'aarista.png',
   'pocketpills.png',
@@ -15,14 +18,49 @@ const companies = [
 </script>
 
 <template>
-  <div class="relative overflow-hidden py-12">
-    <div class="bpinno-container">
-      <p class="font-inter text-sm">Companies we’ve partnered with:</p>
-
-      <div class="flex items-center space-x-20">
-        <!-- <div class="" v-for="company in companies" :key="company">
-          <NuxtImg :src="`/assets/imgs/companies/${company}`" class="max-h-7" />
-        </div> -->
+  <div class="relative overflow-hidden py-8 md:py-10">
+    <div class="bpinno-container flex items-center">
+      <p
+        class="max-xl:hidden font-inter text-sm whitespace-nowrap bg-white z-10 p-2"
+      >
+        Companies we’ve partnered with:
+      </p>
+      <div
+        class="lg:absolute right-0 lg:flex-1 lg:max-w-300 lg:ml-auto overflow-hidden xl:pl-10 max-lg:-mx-6 max-lg:px-6"
+      >
+        <swiper
+          :slides-per-view="2.5"
+          :space-between="48"
+          :loop="true"
+          :centered-slides="true"
+          :breakpoints="{
+            640: {
+              slidesPerView: 4,
+            },
+            1024: {
+              slidesPerView: 5,
+              spaceBetween: 60,
+            },
+            1280: {
+              centeredSlides: false,
+              slidesPerView: 5.5,
+            },
+          }"
+          class="w-full overflow-visible!"
+        >
+          <swiper-slide
+            v-for="company in companies"
+            :key="company"
+            class="flex items-center shrink-0"
+          >
+            <div class="h-6 lg:h-8">
+              <NuxtImg
+                :src="`/assets/imgs/companies/${company}`"
+                class="size-full object-contain brightness-0"
+              />
+            </div>
+          </swiper-slide>
+        </swiper>
       </div>
     </div>
   </div>
